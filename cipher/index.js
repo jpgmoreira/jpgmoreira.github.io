271014c959feee66b0f60f6259e4993e05e30504
joao <jpgmoreira19@gmail.com>
2023-09-30 15:15:36 -0300

    testing

---
+++ cipher/index.js
@@ -0,0 +1,8 @@
+const $senhaEntrada = document.getElementById("senha");
+const $senhaDecifrada = document.getElementById("senha-decifrada");
+const $senhaCifrada = document.getElementById("senha-cifrada");
+
+const decipher = () => {
+    const senhaEntrada = $senhaEntrada.value;
+    console.log(senhaEntrada);
+};
